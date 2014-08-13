8898bdfc4a24c13f24433a0849358b3f5d9c6ce9
Ady Ngom <adyngom.com@gmail.com>
2014-08-12 20:30:53 -0400

    Update image_archetype_instance.js
    A better data abstraction - Removes presentational keys such as class and id which should be left to the client side on what to put there

--- archetypes/image/image_archetype_instance.js
+++ archetypes/image/image_archetype_instance.js
@@ -1,11 +1,37 @@
 module.exports = {
-  src: '',
-  alt: '',
+  description: '',
   title: '',
-  options: {
-    width: '',
-    height: '',
-    class: [],
-    id: ''
-  }
+  img_path: '',
+  sizes: [
+    default: {
+      uri: '',
+      width: '',
+      height: ''
+    },
+    sm: {
+      uri: '',
+      width: '',
+      height: ''
+    },
+    md: {
+      uri: '',
+      width: '',
+      height: ''
+    },
+    lg: {
+      uri: '',
+      width: '',
+      height: ''
+    },
+    x-large: {
+      uri: '',
+      width: '',
+      height: ''
+    },
+    xx-large: {
+      uri: '',
+      width: '',
+      height: ''
+    }
+  ]
 }
