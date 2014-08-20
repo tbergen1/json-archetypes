db8c2abc3830620200adb0ad80ad24ca6eb1b574
Austen Collins <austencollins@gmail.com>
2014-08-20 15:07:46 -0700

    Removed Instantiation

--- index.js
+++ index.js
@@ -14,18 +14,3 @@ archetypes.image = require('./archetypes/image/image_archetype_schema');
 archetypes.task = require('./archetypes/task/task_archetype_schema');
 
 module.exports.archetypes = archetypes;
-
-/**
- * Instantiator
- */
-
-module.exports.new = function(name) {
-	var name = name.toLowerCase();
-	if (typeof name !== 'string') throw new Error('Servant SDK Error - The new() method only accept a string for a name parameter');
-	if (archetypes[name] === 'undefined') throw new Error('Servant SDK Error - You have entered a JSON Archetype that does not exist: ' + name);
-	var instance = {};
-	for (property in archetypes[name].properties) {
-		instance[property] = archetypes[name].properties[property].default;
-	}
-	return instance;
-};
